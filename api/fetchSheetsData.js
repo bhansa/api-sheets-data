d43174dc0cc585b33be612b7d2b2f5080c18b430
bhansa <bsaraswat45@gmail.com>
2021-09-16 21:55:13 +0530

    update fetchSheetsData.js

--- api/fetchSheetsData.js
+++ api/fetchSheetsData.js
@@ -1,12 +1,14 @@
 const google = require('./src/google')
 const http = require('./src/http')
 const {
-    GOOGLE_SHEETS_SHEET_ID
+    GOOGLE_SHEETS_SHEET_ID,
+    GOOGLE_SHEETS_SHEET_RANGE
 } = require('./src/settings')
 
 exports.handler = http.function(async () => {
     const result = await google.sheets.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_SHEET_ID
+        spreadsheetId: GOOGLE_SHEETS_SHEET_ID,
+        range: GOOGLE_SHEETS_SHEET_RANGE
     })
     const [columns, ...rowsOfCells] = result.data.values
     return {
