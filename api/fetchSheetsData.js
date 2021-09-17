fe1a436e0a8e872ae4f89036c7ddf52848d130ac
bhansa <bsaraswat45@gmail.com>
2021-09-17 12:34:26 +0530

    update function

--- api/fetchSheetsData.js
+++ api/fetchSheetsData.js
@@ -1,31 +1,17 @@
 const google = require('./src/google')
 const http = require('./src/http')
 const {
-    GOOGLE_SHEETS_SHEET_ID,
-    GOOGLE_SHEETS_SHEET_RANGE
+  GOOGLE_SHEETS_SHEET_ID,
+  GOOGLE_SHEETS_SHEET_RANGE,
 } = require('./src/settings')
 
 exports.handler = http.function(async () => {
-    const result = await google.sheets.spreadsheets.values.get({
-        spreadsheetId: GOOGLE_SHEETS_SHEET_ID,
-        range: GOOGLE_SHEETS_SHEET_RANGE
-    })
-    const [columns, ...rowsOfCells] = result.data.values
-    return {
-        body : rowsOfCells.map(toRowObject(columns))
-    }
+  const result = await google.sheets.spreadsheets.values.get({
+    spreadsheetId: GOOGLE_SHEETS_SHEET_ID,
+    range: GOOGLE_SHEETS_SHEET_RANGE,
+  })
+  const [columns, ...rowsOfCells] = result.data.values
+  return {
+    body: rowsOfCells
+  }
 })
-
-const toRowObject = (/** @type {string[]} */ columns) => (
-    /** @type {string[]} */ cells
-  ) => {
-    return columns.reduce(
-      (row, column, index) => ({ ...row, [column]: parseValue(cells[index]) }),
-      {}
-    )
-  }
-
-  const parseValue = (value) => {
-    const valueAsNumber = Number(value)
-    return isNaN(valueAsNumber) ? value : valueAsNumber
-  }
