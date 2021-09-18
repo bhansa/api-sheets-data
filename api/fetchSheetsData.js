51e0b264cd5f0dc5824cf41a8190de14f566fc70
bhansa <bsaraswat45@gmail.com>
2021-09-18 10:22:18 +0530

    update api function with service account approach

--- api/fetchSheetsData.js
+++ api/fetchSheetsData.js
@@ -1,17 +1,100 @@
-const google = require('./src/google')
-const http = require('./src/http')
-const {
-  GOOGLE_SHEETS_SHEET_ID,
-  GOOGLE_SHEETS_SHEET_RANGE,
-} = require('./src/settings')
+/*
+ * prerequisites
+ */
+if (!process.env.NETLIFY) {
+  // get local env vars if not in CI
+  // if in CI i expect its already set via the Netlify UI
+  require('dotenv').config();
+}
+// required env vars
+if (!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)
+  throw new Error('no GOOGLE_SERVICE_ACCOUNT_EMAIL env var set');
+if (!process.env.GOOGLE_PRIVATE_KEY)
+  throw new Error('no GOOGLE_PRIVATE_KEY env var set');
+if (!process.env.GOOGLE_SPREADSHEET_ID_FROM_URL)
+  // spreadsheet key is the long id in the sheets URL
+  throw new Error('no GOOGLE_SPREADSHEET_ID_FROM_URL env var set');
 
-exports.handler = http.function(async () => {
-  const result = await google.sheets.spreadsheets.values.get({
-    spreadsheetId: GOOGLE_SHEETS_SHEET_ID,
-    range: GOOGLE_SHEETS_SHEET_RANGE,
-  })
-  const [columns, ...rowsOfCells] = result.data.values
-  return {
-    body: rowsOfCells
+/*
+ * ok real work
+ *
+ * GET /.netlify/functions/google-spreadsheet-fn
+ * GET /.netlify/functions/google-spreadsheet-fn/1
+ * PUT /.netlify/functions/google-spreadsheet-fn/1
+ * POST /.netlify/functions/google-spreadsheet-fn
+ * DELETE /.netlify/functions/google-spreadsheet-fn/1
+ *
+ * the library also allows working just with cells,
+ * but this example only shows CRUD on rows since thats more common
+ */
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+exports.handler = async (event, context) => {
+  const UserIP = event.headers['x-nf-client-connection-ip'] || '6.9.6.9'; // not required, i just feel like using this info
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
+
+  // https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  });
+  await doc.loadInfo(); // loads document properties and worksheets. required.
+  const sheet = doc.sheetsByIndex[0]; // you may want to customize this if you have more than 1 sheet
+  // console.log('accessing', sheet.title, 'it has ', sheet.rowCount, ' rows');
+  const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '');
+  const segments = path.split('/').filter((e) => e);
+
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        /* GET /.netlify/functions/google-spreadsheet-fn */
+        if (segments.length === 0) {
+          const rows = await sheet.getRows(); // can pass in { limit, offset }
+          const serializedRows = rows.map(serializeRow);
+          return {
+            statusCode: 200,
+            // body: JSON.stringify(rows) // dont do this - has circular references
+            body: JSON.stringify(serializedRows) // better
+          };
+        }
+        /* GET /.netlify/functions/google-spreadsheet-fn/123456 */
+        if (segments.length === 1) {
+          const rowId = segments[0];
+          const rows = await sheet.getRows(); // can pass in { limit, offset }
+          const srow = serializeRow(rows[rowId]);
+          return {
+            statusCode: 200,
+            body: JSON.stringify(srow) // just sends less data over the wire
+          };
+        } else {
+          throw new Error(
+            'too many segments in GET request - you should only call somehting like /.netlify/functions/google-spreadsheet-fn/123456 not /.netlify/functions/google-spreadsheet-fn/123456/789/101112'
+          );
+        }
+      /* Fallthrough case */
+      default:
+        return {
+          statusCode: 500,
+          body: 'unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE'
+        };
+    }
+  } catch (err) {
+    console.error('error ocurred in processing ', event);
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: err.toString()
+    };
   }
-})
+
+  /*
+   * utils
+   */
+  function serializeRow(row) {
+    let temp = {};
+    sheet.headerValues.map((header) => {
+      temp[header] = row[header];
+    });
+    return temp;
+  }
+};
